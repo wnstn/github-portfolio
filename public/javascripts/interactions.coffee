3e9d055caa5d6e0257600db83fdbf371be32b7e4
Winston Hearn <winston@wnstn.com>
2013-09-11 09:52:49 -0700

    Adding test scroll code

--- public/javascripts/interactions.coffee
+++ public/javascripts/interactions.coffee
@@ -1,8 +1,25 @@
-$ ->
-  showThings = ()->
-    body = $('body')
-    body.addClass('live')
-    window.clearTimeout(delay)
+Portfolio = {}
+
+class Portfolio.Nav
+
+  constructor: ->
+    @mobile = false if document.width > 480 then true
+    console.log @mobile
+    @body = document.getElementsByTagName('body')
+    @body = @body[0]
+    window.onScroll = @checkOffset()
+
+  checkOffset: () =>
+    console.log "offset top", @body.offsetTop
+    if @body.offset
+      console.log "true"
+      @toggleMenu()
+
+  toggleMenu: () =>
+    console.log "toggling"
+    if @mobile is true
+      nav = document.getElementById('nav-wrapper')
+      console.log "nav", nav
 
 
-  delay = window.setTimeout(showThings, 300);
+Portfolio.Nav = new Portfolio.Nav
--- public/javascripts/interactions.js
+++ public/javascripts/interactions.js
@@ -1,14 +1,38 @@
 (function() {
-  $(function() {
-    var delay, showThings;
-    showThings = function() {
-      var body;
-      body = $('body');
-      body.addClass('live');
-      return window.clearTimeout(delay);
+  var Portfolio,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+  Portfolio = {};
+
+  Portfolio.Nav = (function() {
+    function Nav() {
+      this.toggleMenu = __bind(this.toggleMenu, this);
+      this.checkOffset = __bind(this.checkOffset, this);
+      this.body = document.getElementsByTagName('body');
+      console.log(this.body);
+      window.onScroll = this.checkOffset();
+    }
+
+    Nav.prototype.checkOffset = function() {
+      console.log(this.body.offset);
+      if (this.body.offset) {
+        console.log("true");
+        return this.toggleMenu();
+      }
     };
-    return delay = window.setTimeout(showThings, 300);
-  });
+
+    Nav.prototype.toggleMenu = function() {
+      console.log("toggling");
+      if (document.width < 481) {
+        return $('#nav-wrapper').toggleClass('fixed');
+      }
+    };
+
+    return Nav;
+
+  })();
+
+  Portfolio.Nav = new Portfolio.Nav;
 
 }).call(this);
 
